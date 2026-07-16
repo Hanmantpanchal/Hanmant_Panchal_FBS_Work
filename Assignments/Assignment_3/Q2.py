3515cb9f21d89b85d08461f81688a7418dc48bf2
Hanmant panchal <123748781+Hanmantpanchal@users.noreply.github.com>
2026-07-16 09:28:03 +0530

    Delete Assignments directory

--- Assignments/Assignment_3/Q2.py
+++
@@ -1,9 +0,0 @@
-#2. Write a program to input any alphabet and check whether it is vowel or consonant.
-
-ch = input("Enter a character : ")
-
-
-if(ch is 'aeiouAEIOU'):
-    print("It is a vowel")
-else:
-    print("It is a consonant")
--- Assignments/Assignment_3/Q7.py
+++
@@ -1,9 +0,0 @@
-#7. Write a program to check if user has entered correct userid and password.
-
-userid = input("Enter your userid: ")
-password = input("Enter your password: ")
-
-if((userid == "admin") and (password == "admin123")):
-    print("Login Successful")
-else:
-    print("Invalid Credentials")
